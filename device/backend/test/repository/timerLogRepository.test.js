5be66033f5f1df822b29c506f1e3535b4bdf7330
김상현 <shkim7876@naver.com>
2023-07-25 14:07:52 +0900

    feat: timerLogRepository now can commit & rollback
    + methods in timerLogRepository now optionally receives connection and
      timestamp.
      connection can be used to implement commit & rollback operation.
      timestamp can can be used to manually set time for logging.
    + added datetimeDTO that can be used for timestamp.

--- device/backend/test/repository/timerLogRepository.test.js
+++ device/backend/test/repository/timerLogRepository.test.js
@@ -35,10 +35,43 @@ describe("database management", () => {
     const promise = timerLogRepository.start(-1);
     await promise
       .then(() => {
-        fail("do_operation does not throw anything.");
+        throw "do_operation did not throw anything.";
       })
       .catch((e) => {
         expect(e).toMatch(/^SQLError:/);
       });
   });
+
+  test("find All works", async() =>{
+    console.log(await timerLogRepository.findAll());
+  });
 });
+
+describe("Security", ()=>{
+  beforeEach(()=>{
+    timerLogRepository = new TimerLogRepository();
+    timerRepository = new TimerRepository();
+  })
+  afterEach(async ()=>{
+    await timerLogRepository.closeDB();
+    timerLogRepository = null;
+    await timerRepository.closeDB();
+    timerRepository = null;
+  })
+  test("timerLogRepository is safe from SQL injection", async ()=>{
+    await timerRepository.registTimer("09:00:00", "18:00:00", "종일");
+    const [rows, fields] = await timerRepository.findAll();
+    expect(rows.length).toBeGreaterThanOrEqual(1);
+    const pid = rows[0].timer_id;
+    try{
+      await timerLogRepository.start(
+        `${pid});DELETE FROM timer_log WHERE 1=1;-- `
+      );
+    }
+    catch(e){
+      expect(e).toMatch(/^SQLError: Cannot insert into database,/);
+    }
+    //const [logRow, logFields] = await timerLogRepository.findByTimerId(pid);
+    //expect(logRow.length).toBeGreaterThanOrEqual(1);
+  });
+})
