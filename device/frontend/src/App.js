32b232e38b1a7d06446e2e077859f7538853412f
박정훈 <ggongjja0@naver.com>
2023-07-25 13:53:31 +0900

    style: timer component css 추가

--- device/frontend/src/App.js
+++ device/frontend/src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { Link,  Routes , Route } from 'react-router-dom';
-import BasicTimerComponent from './pages/basicTimerComponent';
-import CreateTimerPage from './pages/createTimerPage';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import BasicTimerComponent from "./pages/basicTimerComponent";
+import CreateTimerPage from "./pages/createTimerPage";
 
 //라우팅 페이지 관련
-import Page1 from './pages/page1';
-import Page2 from './pages/page2';
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
 
 function App() {
   return (
     <div className="App">
-      <CreateTimerPage/>
-      <BasicTimerComponent/>
-      <BasicTimerComponent/>
-      <BasicTimerComponent/>
-      <BasicTimerComponent/>
+      <CreateTimerPage />
+      <BasicTimerComponent />
+      <BasicTimerComponent />
+      <BasicTimerComponent />
+      <BasicTimerComponent />
       <Link to="/page1">페이지1라우팅 |</Link>
       <Link to="/page2">페이지2라우팅</Link>
-      
+
       <Routes>
-        <Route path="/page1" element = {<Page1/>}> </Route>
-        <Route path="/page2" element = {<Page2/>} ></Route>
+        <Route path="/page1" element={<Page1 />}>
+          {" "}
+        </Route>
+        <Route path="/page2" element={<Page2 />}></Route>
       </Routes>
 
       <header className="App-header">
--- device/frontend/src/pages/basicTimerComponent.js
+++ device/frontend/src/pages/basicTimerComponent.js
@@ -1,42 +1,68 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import BasicTimer from "../timerTestDir/basic_timer";
+import "./basicTimer.css";
 
-function useConstructor(callBack = () => { }) {
-    const flag = useRef(false);
-    if (flag.current) return;
-    callBack();
-    flag.current = true;
+// mui
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+function useConstructor(callBack = () => {}) {
+  const flag = useRef(false);
+  if (flag.current) return;
+  callBack();
+  flag.current = true;
 }
 
-function TimerCreatePage() {
+const BasicTimerComponent = ({ timer }) => {
+  const [value, setValue] = useState(0);
+  const refTimer = useRef(null);
+  const refText = useRef(null);
 
-    const [value, setValue] = useState(0);
-    const refTimer = useRef(null);
-    const refText = useRef(null);
+  useConstructor(() => {
+    refTimer.current = new BasicTimer("", setValue);
+  });
 
-    useConstructor(() => {
-        refTimer.current = new BasicTimer("", setValue);
-    });
+  function resume() {
+    const timer = refTimer.current;
+    timer.isRunning ? timer.pause() : timer.start();
+  }
 
-    function resume() {
-        const timer = refTimer.current;
-        timer.isRunning ? timer.pause() : timer.start();
-    }
+  function reset() {
+    const timer = refTimer.current;
+    timer.reset(refText.current * 1000);
+  }
 
-    function reset() {
-        const timer = refTimer.current;
-        timer.reset(refText.current * 1000);
-    }
-
-    return (
-        <div>
-            <button onClick={resume}> {value} </button>
-            <button onClick={reset}> reset </button>
-            <input type="text"
-                placeholder="타이머 시간 설정"
-                onChange={(arg) => { refText.current = arg.target.value }}></input>
-        </div>
-    )
-}
-export default TimerCreatePage
-
+  return (
+    <Box className="timer">
+      <Grid container justifyContent={"center"} alignContent={"center"}>
+        <Grid item xs={6} className="time">
+          {("00" + Math.floor(value / 1000 / 3600)).slice(-2)}:{" "}
+          {("00" + Math.floor(value / 1000 / 60)).slice(-2)} :{" "}
+          {("00" + Math.floor((value / 1000) % 60)).slice(-2)}
+          <span className="micro">
+            {("00" + Math.floor(((value / 1000) % 1) * 100)).slice(-2)}
+          </span>
+        </Grid>
+        <Grid item xs={4} className="timerButton">
+          <button className="start" onClick={resume}>
+            {"▶"}
+          </button>
+          <button className="reset" onClick={reset}>
+            {value == 0 ? "set" : "reset"}
+          </button>
+        </Grid>
+        <Grid item xs={12}>
+          <input
+            className="inputTime"
+            type="text"
+            placeholder="타이머 시간 설정"
+            onChange={(arg) => {
+              refText.current = arg.target.value;
+            }}
+          ></input>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+export default BasicTimerComponent;
