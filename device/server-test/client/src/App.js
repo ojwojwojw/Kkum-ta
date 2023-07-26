979d22d51ea148c169288c6e0386203f5cf20224
kunho <didrjsgh11@naver.com>
2023-07-26 09:59:15 +0900

    Feat: Timer Log Data to Ract State Variable
    Body:
    DB의 timer_log DB 데이터를 back-server에 표시
    proxy를 통해 front-server에서도 표시 가능
    axois 함수로 server에서 get 데이터를 state 변수로 표시 완료

--- device/server-test/client/src/App.js
+++ device/server-test/client/src/App.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [timers, updateArr] = useState([]);
+    const [logs, updateLog] = useState([]);
 
     useEffect(() => {
-        const fetchDatas = async () => {
-            await axios.get("/timer_table").then((res) => {
+        const fetchDatas = () => {
+            axios.get("/timer_table").then((res) => {
                 updateArr(res.data);
             });
         };
@@ -17,17 +18,29 @@ function App() {
         };
     }, []);
 
+    const getLogs = async () => {
+        try {
+            const res = await axios.get("/timer_log");
+            updateLog(res.data);
+        } catch (error) {
+            console.error("Error occured during get data: ", error);
+        }
+    };
+
     return (
         <div>
-          {
-            timers.map((timer) => (
-              <div>
-                <div>이름 : {timer.timer_name}</div>
-                <div>시작 시간 : {timer.start_time}</div>
-                <div>종료 시간 : {timer.end_time}</div>
-              </div>
-            ))
-          }
+            <button onClick={getLogs}>click here!</button>
+            {timers.map((timer) => (
+                <div>
+                    <div>이름 : {timer.timer_name}</div>
+                    <div>시작 시간 : {timer.start_time}</div>
+                    <div>종료 시간 : {timer.end_time}</div>
+                </div>
+            ))}
+
+            {logs.map((item) => (
+              <div key={item.timer_log_id}>{item.operation}</div>
+            ))}
             ...
         </div>
     );
