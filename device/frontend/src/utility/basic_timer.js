8ba6e3e004c303a2b6d16c69a146ddc7216e0722
김민겸 <lqaks456@gmail.com>
2023-07-30 23:15:47 +0900

    fix: change target time to private member variable

--- device/frontend/src/utility/basic_timer.js
+++ device/frontend/src/utility/basic_timer.js
@@ -1,6 +1,7 @@
 export default class BasicTimer {
   #counter;
   #isRunning;
+  #target;
   #time;
   #initTime;
   #tagTime;
@@ -8,13 +9,14 @@ export default class BasicTimer {
   #maxIter;
   #iter;
   constructor() {
-    this.#initTime = [10 * 1000, 15 * 1000, 12 * 1000];
+    this.#initTime = [1 * 1000, 5 * 1000, 2 * 1000];
     this.#tagTime = this.#initTime;
+    this.#target = 0;
     this.#time = this.#initTime[0];
     this.#idx = 0;
     this.#isRunning = false;
     this.#iter = 0;
-    this.#maxIter = 0;
+    this.#maxIter = 2;
     this.setTime = null;
     this.setIsRunning = null;
     this.setProgress = null;
@@ -24,29 +26,27 @@ export default class BasicTimer {
   #determine() {
     if (this.#idx + 1 < this.#initTime.length) {
       this.#time = this.#initTime[++this.#idx];
-      this.start();
+      this.#target = new Date().getTime() + this.#time;
       return;
     }
     if (this.#maxIter == 0 || this.#iter + 1 < this.#maxIter) {
       this.#time = this.#initTime[0];
       this.#idx = 0;
-      if(this.#maxIter > 0) this.#iter++;
-      this.start();
+      if (this.#maxIter > 0) this.#iter++;
+      this.#target = new Date().getTime() + this.#time;
       return;
     }
+    clearInterval(this.#counter);
+    this.#isRunning = false;
+    if (this.setIsRunning != null) this.setIsRunning(false);
     this.#time = 0;
   }
 
-  #count(end) {
+  #count() {
     this.#counter = setInterval(() => {
       const now = new Date().getTime();
-      this.#time = end - now;
-      if (this.#time <= 0) {
-        clearInterval(this.#counter);
-        this.#isRunning = false;
-        if (this.setIsRunning != null) this.setIsRunning(false);
-        this.#determine();
-      }
+      this.#time = this.#target - now;
+      if (this.#time <= 0) this.#determine();
       if (this.setTime != null) this.setTime(this.#time);
       if (this.setProgress != null) this.setProgress(1 - this.#time / this.#initTime[this.#idx]);
     }, 31);
@@ -56,8 +56,8 @@ export default class BasicTimer {
     if (this.#isRunning) return;
     this.#isRunning = true;
     if (this.setIsRunning != null) this.setIsRunning(true);
-    const end = new Date().getTime() + this.#time;
-    this.#count(end);
+    this.#target = new Date().getTime() + this.#time;
+    this.#count();
     console.log(`start: ${this.#time}`);
   }
 
