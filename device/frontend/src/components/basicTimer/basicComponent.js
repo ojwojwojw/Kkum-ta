be1c5dd26437b342db81722ee971b8b6ec66abbd
박정훈 <ggongjja0@naver.com>
2023-07-31 09:55:50 +0900

    Merge: 'divide_timer_stopWatch' to 'develop'

--- device/frontend/src/components/basicTimer/basicComponent.js
+++ device/frontend/src/components/basicTimer/basicComponent.js
@@ -4,7 +4,7 @@ import React, { useState, useRef, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import "./basicComponent.css";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 
 import PauseIcon from "@mui/icons-material/Pause";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
@@ -13,24 +13,29 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import { Button, IconButton } from "@mui/material";
 import Numpad from "./numpad";
 
-function useConstructor(callBack = () => { }) {
+function useConstructor(callBack = () => {}) {
   const flag = useRef(false);
   if (flag.current) return;
   callBack();
   flag.current = true;
 }
 
-export default function BasicTimerComponent({ timer, idx, type, removeTimer, WatchId  }) {
-
+export default function BasicTimerComponent({
+  timer,
+  idx,
+  type,
+  removeTimer,
+  WatchId,
+}) {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [input, setInput] = useState(0);
 
   useEffect(() => {
-    if(WatchId) {
-      console.log("New timer Id:",WatchId)
+    if (WatchId) {
+      // console.log("New timer Id:", WatchId);
     }
-  },[WatchId])
+  }, [WatchId]);
 
   useConstructor(() => {
     timer.setTime = setTime;
@@ -43,21 +48,31 @@ export default function BasicTimerComponent({ timer, idx, type, removeTimer, Wat
   }
 
   function reset() {
-    console.log(timer)
+    // console.log(timer);
     timer.reset(input * 1000);
   }
 
   function remove() {
-    removeTimer(WatchId)
+    removeTimer(WatchId);
   }
 
   return (
-    <Box container className={type=="timer"?"watch timer":"watch stopWatch"}>
-      <Grid container xs={100} justifyContent={"center"} alignContent={"center"}>
-        <Grid xs={1}><h2>{idx+1}</h2></Grid>
+    <Box
+      container
+      className={type == "timer" ? "watch timer" : "watch stopWatch"}
+    >
+      <Grid
+        container
+        xs={100}
+        justifyContent={"center"}
+        alignContent={"center"}
+      >
+        <Grid xs={1}>
+          <h2>{idx + 1}</h2>
+        </Grid>
         <Grid item xs={6} className="time">
           {("00" + Math.floor(time / 1000 / 3600)).slice(-2)}:{" "}
-          {("00" + Math.floor((time / 1000) % 3600 / 60)).slice(-2)} :{" "}
+          {("00" + Math.floor(((time / 1000) % 3600) / 60)).slice(-2)} :{" "}
           {("00" + Math.floor((time / 1000) % 60)).slice(-2)}
           <span className="micro">
             {("00" + Math.floor(((time / 1000) % 1) * 100)).slice(-2)}
@@ -80,14 +95,21 @@ export default function BasicTimerComponent({ timer, idx, type, removeTimer, Wat
           </Button>
         </Grid>
         <Grid>
-          <IconButton  ton aria-label="delete" variant="text" color="error" size="large" onClick={remove}>
+          <IconButton
+            ton
+            aria-label="delete"
+            variant="text"
+            color="error"
+            size="large"
+            onClick={remove}
+          >
             <CloseIcon />
-          </IconButton >
+          </IconButton>
         </Grid>
       </Grid>
 
       {/* 넘패드 컴포넌트로 분리 */}
-      <Numpad input={input} setInput={setInput} WatchId={WatchId}/>
+      <Numpad input={input} setInput={setInput} WatchId={WatchId} />
       {/*
       <Grid container xs={20} justifyContent={"center"} alignContent={"center"}>
         <Grid item xs={20}>
@@ -111,7 +133,6 @@ export default function BasicTimerComponent({ timer, idx, type, removeTimer, Wat
           </div>
         </Grid>
       </Grid> */}
-
-    </Box >
+    </Box>
   );
 }
--- device/frontend/src/components/basicTimer/basicContainer.js
+++ device/frontend/src/components/basicTimer/basicContainer.js
@@ -3,40 +3,39 @@ import React, { useEffect, useRef, useState } from "react";
 import BasicTimer from "../../utility/basic_timer";
 import BasicStopwatch from "../../utility/basic_stopwatch";
 import BasicTimerComponent from "./basicComponent";
-import TransitionsModal from "./CreateModal"
+import TransitionsModal from "./CreateModal";
 import axios from "axios";
 
-import './basicContainer.css'
+import "./basicContainer.css";
 
 export default function TimerContainer() {
-
   const [timerList, setTimerList] = useState([]);
   const input = useRef(0);
 
   useEffect(() => {
     console.log("timer container constructor");
 
-    return (() => {
+    return () => {
       console.log("timer container destructor");
-    })
+    };
   }, []);
 
   // 타이머 스톱워치 생성 함수 리팩토링(중복 제거 후 타입으로 구분)
-  function createBasicWatch(type,idx) {
-    if (timerList.length>=30) return;
+  function createBasicWatch(type, idx) {
+    if (timerList.length >= 30) return;
 
     const newWatch = {
-      id:Date.now(),
+      id: Date.now(),
       type: type,
       timer: type === "timer" ? new BasicTimer() : new BasicStopwatch(),
     };
-    console.log(newWatch);
+    // console.log(newWatch);
     setTimerList((prevTimerList) => {
       const newList = [...prevTimerList];
-      newList.splice(idx,0, newWatch);
+      newList.splice(idx, 0, newWatch);
       return newList;
-    })
-    return newWatch.id
+    });
+    return newWatch.id;
   }
 
   // function createBasicTimer(idx) {
@@ -61,15 +60,14 @@ export default function TimerContainer() {
 
   function remove(id) {
     if (timerList.length == 0) return;
-    let deleteIdx = 0
+    let deleteIdx = 0;
     timerList.forEach((obj, idx) => {
-
-      if(obj.id === id)  {
-        deleteIdx = idx
+      if (obj.id === id) {
+        deleteIdx = idx;
       }
-    })
-    
-    setTimerList(prevTimerList => {
+    });
+
+    setTimerList((prevTimerList) => {
       const newList = [...prevTimerList];
       newList.splice(deleteIdx, 1);
       return newList;
@@ -83,14 +81,14 @@ export default function TimerContainer() {
   }
 
   function save() {
-    // time, init 
+    // time, init
     const arr = [];
-    timerList.forEach(obj => {
+    timerList.forEach((obj) => {
       const timer = obj.timer;
       const data = timer.save();
       data.type = obj.type;
       arr.push(data);
-    })
+    });
     console.log(arr);
   }
 
@@ -108,12 +106,20 @@ export default function TimerContainer() {
 
   return (
     <>
-      <span style={{margin : "10px"}}>
-        <TransitionsModal createWatch={createBasicWatch}/>
-        <button onClick={() => createBasicWatch(input.type="timer",input.current)}>
+      <span style={{ margin: "10px" }}>
+        <TransitionsModal createWatch={createBasicWatch} />
+        <button
+          onClick={() =>
+            createBasicWatch((input.type = "timer"), input.current)
+          }
+        >
           create timer
         </button>
-        <button onClick={() => createBasicWatch(input.type="stopWatch",input.current)}>
+        <button
+          onClick={() =>
+            createBasicWatch((input.type = "stopWatch"), input.current)
+          }
+        >
           create stopwatch
         </button>
         {/* <button onClick={() => remove(input.current)}>
@@ -125,41 +131,30 @@ export default function TimerContainer() {
           placeholder="인덱스 입력">
         </input> */}
       </span>
-      <span style={{margin : "10px"}}>
-        <button onClick={() => load()}>
-          load
-        </button>
-        <button onClick={() => save()}>
-          save
-        </button>
+      <span style={{ margin: "10px" }}>
+        <button onClick={() => load()}>load</button>
+        <button onClick={() => save()}>save</button>
       </span>
-      <span style={{margin : "10px"}}>
-        <button onClick={() => allStart()}>
-          all start
-        </button>
-        <button onClick={() => allPause()}>
-          all pause
-        </button>
-        <button onClick={() => allReset()}>
-          all reset
-        </button>
-        
+      <span style={{ margin: "10px" }}>
+        <button onClick={() => allStart()}>all start</button>
+        <button onClick={() => allPause()}>all pause</button>
+        <button onClick={() => allReset()}>all reset</button>
       </span>
       <div>
-        {
-          timerList.map((obj, idx) => {
-            console.log(`timer ${idx}`)
-            return (<BasicTimerComponent 
+        {timerList.map((obj, idx) => {
+          console.log(`timer ${idx}`);
+          return (
+            <BasicTimerComponent
               key={obj.id}
               timer={obj.timer}
               idx={idx}
               type={obj.type}
               removeTimer={remove}
               WatchId={obj.id}
-              />);
-          })
-        }
+            />
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
