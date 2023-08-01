df7527ba34c78630329625ef3f415f53998228fe
박정훈 <ggongjja0@naver.com>
2023-08-01 11:06:53 +0900

    feat: Create group tab and merge operations
    + 그룹생성 기능으로 생긴 버튼을 탭 버튼으로 생성

--- device/frontend/src/App.js
+++ device/frontend/src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Grid, Box } from "@mui/material";
-=======
-import { BrowserRouter, Routes, Route, Link, RouterProvider } from 'react-router-dom';
 
 import GroupComponent from "./components/basicTimer/groupComponent";
->>>>>>> develop
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Box>
-        <Grid>
-          <BasicContainer />
-        </Grid>
-      </Box>
-=======
       <GroupComponent />
->>>>>>> develop
     </div>
   );
 }
