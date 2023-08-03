71e73253afc3503f9693070fb3e7128372cd427e
김상현 <shkim7876@naver.com>
2023-08-03 17:40:35 +0900

    feat: ComponentLogRepository
    + added ComponentLogRepository that manages Logging.

--- device/backend/src/repository/groupRepository.js
+++ device/backend/src/repository/groupRepository.js
@@ -9,6 +9,7 @@ class GroupRepository extends Repository{
         CREATE TABLE IF NOT EXISTS group_tbl (
             group_key INT(11) NOT NULL,
             name VARCHAR(50) NULL DEFAULT '기본' COLLATE 'utf8mb4_general_ci',
+            last_update TIMESTAMP NULL DEFAULT current_timestamp(),
             PRIMARY KEY (group_key) USING BTREE
         )
         COLLATE='utf8mb4_general_ci'
