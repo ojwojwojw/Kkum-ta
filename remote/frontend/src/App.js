3f0a528610cfc77a7b8d7d72eb1bd940085d43ac
박정훈 <ggongjja0@naver.com>
2023-08-04 11:54:23 +0900

    feat: mainpage layout menu 구성 및 dopdown 추가

--- remote/frontend/src/App.js
+++ remote/frontend/src/App.js
@@ -1,19 +1,75 @@
 import React, { useState } from "react";
 import "./App.css";
-import CustomTabPanel from "./components/CustomTabPanel";
+import MenuListBar from "./components/MenuListBar";
 import Login from "./components/login";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Grid, Button, Menu, MenuItem } from "@mui/material";
 
 function App() {
   const [isAthenticated, setIsAthenticated] = useState(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Link to="/">Main</Link>
-        <Link to="/login">Login</Link>
+        {isAthenticated && (
+          <Grid
+            container
+            className="navbar"
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Grid item xs={2}>
+              <Link to="/">LOGO</Link>
+            </Grid>
+            <Grid item xs={8}></Grid>
+            <Grid item xs={2}>
+              {!isAthenticated ? (
+                <>
+                  <Button
+                    id="user-button"
+                    aria-controls={open ? "user-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={handleClick}
+                  >
+                    UserName
+                  </Button>
+                  <Menu
+                    id="user-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "user-button",
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>MyPage</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </Grid>
+          </Grid>
+        )}
         <Routes>
-          <Route exact path="/" Component={!isAthenticated ? Login : CustomTabPanel}></Route>
-          <Route exact path="login" Component={Login}/>
+          <Route
+            exact
+            path="/"
+            Component={!isAthenticated ? Login : MenuListBar}
+          ></Route>
+          <Route exact path="login" Component={Login} />
+          <Route exact path="reports" Component={""} />
         </Routes>
       </div>
     </BrowserRouter>
