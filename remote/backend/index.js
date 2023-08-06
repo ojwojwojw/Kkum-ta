153e420284ad2dfc50652280311299c09e2bdd63
seongmin15 <tjdalsdl19@gmail.com>
2023-08-06 21:49:32 +0900

    style: remove express-session part

--- remote/backend/index.js
+++ remote/backend/index.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const passportConfig = require("./src/passport");
-const session = require("./src/service/sessionService");
 const cookieParser = require("cookie-parser");
 const PORT = 8080;
 
@@ -11,9 +10,7 @@ const authController = require("./src/controller/authController");
 
 passportConfig(passport);
 
-//app.use(session);
 app.use(passport.initialize());
-//app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/auth", authController);
