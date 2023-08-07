768f107e66d36d5be8f70b0b9e3e2d9a944be32a
박정훈 <ggongjja0@naver.com>
2023-08-07 17:26:00 +0900

    feat : 페이지 라우팅 통계 페이지 잔디 생성

--- remote/frontend/src/components/MenuListBar.js
+++ remote/frontend/src/components/MenuListBar.js
@@ -11,89 +11,120 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import { Link } from "react-router-dom";
 
+import { Grid } from "@mui/material";
+
 export default function MenuListBar() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
   return (
-    <List
-      sx={{
-        width: "14rem",
-        minHeight: "1000px",
-        bgcolor: "background.paper",
-      }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-    >
-      <ListItemButton component={Link} to="/">
-        <ListItemIcon>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary="통계" />
-      </ListItemButton>
+    <Grid item xs={2}>
+      <List
+        sx={{
+          top: "50px",
+          left: 0,
+          position: "fixed",
+          width: "200px",
+          minHeight: "520px",
+          bgcolor: "background.paper",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        <ListItemButton component={Link} to="/reports">
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="통계" />
+        </ListItemButton>
 
-      <ListItemButton onClick={handleClick}>
-        <ListItemIcon>
-          <SettingsIcon />
-        </ListItemIcon>
-        <ListItemText primary="그룹관리" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton
-            sx={{
-              pl: 4,
-              bgcolor: "#fafafa",
-              borderBottom: "2px solid #cacaca",
-            }}
-          >
-            <ListItemIcon>
-              <AccessTimeIcon />
-            </ListItemIcon>
-            <ListItemText primary="GROUP 1" />
-          </ListItemButton>
-          <ListItemButton
-            sx={{
-              pl: 4,
-              bgcolor: "#fafafa",
-              borderBottom: "2px solid #cacaca",
-            }}
-          >
-            <ListItemIcon>
-              <AccessTimeIcon />
-            </ListItemIcon>
-            <ListItemText primary="GROUP 2" />
-          </ListItemButton>
-          <ListItemButton
-            sx={{
-              pl: 4,
-              bgcolor: "#fafafa",
-              borderBottom: "2px solid #cacaca",
-            }}
-          >
-            <ListItemIcon>
-              <AccessTimeIcon />
-            </ListItemIcon>
-            <ListItemText primary="GROUP 3" />
-          </ListItemButton>
-          <ListItemButton
-            sx={{
-              pl: 4,
-              bgcolor: "#fafafa",
-              borderBottom: "2px solid #cacaca",
-            }}
-          >
-            <ListItemIcon>
-              <AccessTimeIcon />
-            </ListItemIcon>
-            <ListItemText primary="GROUP 4" />
-          </ListItemButton>
-        </List>
-      </Collapse>
-    </List>
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="그룹관리" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton
+              component={Link}
+              to="/group1"
+              sx={{
+                pl: 4,
+                bgcolor: "#fafafa",
+                borderBottom: "2px solid #cacaca",
+              }}
+            >
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText primary="GROUP 1" />
+            </ListItemButton>
+            <ListItemButton
+              component={Link}
+              to="/group2"
+              sx={{
+                pl: 4,
+                bgcolor: "#fafafa",
+                borderBottom: "2px solid #cacaca",
+              }}
+            >
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText primary="GROUP 2" />
+            </ListItemButton>
+            <ListItemButton
+              component={Link}
+              to="/group3"
+              sx={{
+                pl: 4,
+                bgcolor: "#fafafa",
+                borderBottom: "2px solid #cacaca",
+              }}
+            >
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText primary="GROUP 3" />
+            </ListItemButton>
+            <ListItemButton
+              component={Link}
+              to="/group4"
+              sx={{
+                pl: 4,
+                bgcolor: "#fafafa",
+                borderBottom: "2px solid #cacaca",
+              }}
+            >
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText primary="GROUP 4" />
+            </ListItemButton>
+            <ListItemButton
+              component={Link}
+              to="/group5"
+              sx={{
+                pl: 4,
+                bgcolor: "#fafafa",
+                borderBottom: "2px solid #cacaca",
+              }}
+            >
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText primary="GROUP 5" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+      </List>
+    </Grid>
   );
 }
