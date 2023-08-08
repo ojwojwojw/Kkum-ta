bc5734c35f8d88b60771aa87484feedb0cc67958
김성민 <tjdalsdl19@gmail.com>
2023-08-08 15:46:15 +0900

    fix: fix cors error

--- remote/backend/index.js
+++ remote/backend/index.js
@@ -13,9 +13,16 @@ const devController = require("./src/controller/devController");
 
 passportConfig(passport);
 
+const whitelist = ["http://localhost:3000"];
 const corsOptions = {
-  origin: "*", // 출처 허용 옵션
-  credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
 app.use(cors(corsOptions));
