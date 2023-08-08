f7f59ca95f64d20d96a757c2b0a0376467dbc2f3
김성민 <tjdalsdl19@gmail.com>
2023-08-08 15:42:15 +0900

    fix: cors error fix

--- remote/backend/index.js
+++ remote/backend/index.js
@@ -13,31 +13,48 @@ const devController = require("./src/controller/devController");
 
 passportConfig(passport);
 
+const whitelist = ["http://localhost:3000"];
 const corsOptions = {
-    origin: 'https://localhost:3000',      // 출처 허용 옵션
-    credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
-}
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 app.use(cors(corsOptions));
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRETE,
     resave: true,
     saveUninitialized: true,
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 const debug = (req, res, next) => {
-    const beginTime = Date.now();
-    originalJSON = res.json;
-    res.json = function (data) {
-        console.log(req.method + " " + req.path + ", body:" + JSON.stringify(req.body) + ", query:" + JSON.stringify(req.query));
-        console.log(`\t${JSON.stringify(data)}`);
-        console.log(`ellapsed time: ${Date.now() - beginTime} ms\n`);
-        originalJSON.call(this, data);
-    }
-    next();
+  const beginTime = Date.now();
+  originalJSON = res.json;
+  res.json = function (data) {
+    console.log(
+      req.method +
+        " " +
+        req.path +
+        ", body:" +
+        JSON.stringify(req.body) +
+        ", query:" +
+        JSON.stringify(req.query)
+    );
+    console.log(`\t${JSON.stringify(data)}`);
+    console.log(`ellapsed time: ${Date.now() - beginTime} ms\n`);
+    originalJSON.call(this, data);
+  };
+  next();
 };
 
 app.use(debug);
