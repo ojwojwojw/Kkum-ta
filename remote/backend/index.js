53389ad60d7117bf286d40f59b5b8b60e686ab06
seongmin15 <tjdalsdl19@gmail.com>
2023-08-09 00:18:56 +0900

    feat: Add naver login

--- remote/backend/index.js
+++ remote/backend/index.js
@@ -14,19 +14,19 @@ const logController = require("./src/controller/logController");
 
 passportConfig(passport);
 
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const whitelist = ["http://localhost:3000"];
+// const corsOptions = {
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(
   session({
     secret: process.env.SESSION_SECRETE,
--- remote/backend/src/passport/naverStrategy.js
+++ remote/backend/src/passport/naverStrategy.js
@@ -1,22 +1,22 @@
 const passport = require("passport");
-const KakaoStrategy = require("passport-kakao").Strategy;
+const NaverStrategy = require("passport-naver-v2").Strategy;
 const loginService = require("../service/signupService");
 const loginApp = new loginService();
 const UserRepository = require("../repository/userRepository");
 
 module.exports = () => {
   passport.use(
-    new KakaoStrategy(
+    new NaverStrategy(
       {
-        clientID: process.env.KAKAO_ID, // 카카오 로그인에서 발급받은 REST API 키
-        callbackURL: "/auth/kakao/callback", // 카카오 로그인 Redirect URI 경로
-        clientSecret: process.env.KAKAO_CLIENT_SECRETE,
+        clientID: process.env.NAVER_ID, // 네이버 로그인에서 발급받은 REST API 키
+        callbackURL: "/auth/naver/callback", // 네이버 로그인 Redirect URI 경로
+        clientSecret: process.env.NAVER_CLIENT_SECRETE,
       },
       /*
-       * clientID에 카카오 앱 아이디 추가
-       * callbackURL: 카카오 로그인 후 카카오가 결과를 전송해줄 URL
-       * accessToken, refreshToken: 로그인 성공 후 카카오가 보내준 토큰
-       * profile: 카카오가 보내준 유저 정보. profile의 정보를 바탕으로 회원가입
+       * clientID에 네이버 앱 아이디 추가
+       * callbackURL: 네이버 로그인 후 네이버가 결과를 전송해줄 URL
+       * accessToken, refreshToken: 로그인 성공 후 구글이 보내준 토큰
+       * profile: 네이버가 보내준 유저 정보. profile의 정보를 바탕으로 회원가입
        */
       async (accessToken, refreshToken, profile, done) => {
         try {
@@ -25,7 +25,7 @@ module.exports = () => {
             profile.id,
             profile.provider
           );
-          // 이미 가입된 카카오 프로필이면 성공
+          // 이미 가입된 네이버 프로필이면 성공
           if (exUser) {
             done(null, exUser); // 로그인 인증 완료
           } else {
@@ -35,6 +35,7 @@ module.exports = () => {
               profile.id,
               profile.provider
             );
+            console.log(newUser);
             done(null, newUser); // 회원가입하고 로그인 인증 완료
           }
         } catch (error) {
