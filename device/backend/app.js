6bd7c3181456f8528223b646d1a1da66592a9026
김상현 <shkim7876@naver.com>
2023-08-10 22:16:55 +0900

    feat: swagger for /dev and /stopwatch

--- device/backend/app.js
+++ device/backend/app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+const options = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            version: "0.1.0",
+            title: "KKumta",
+            description: "꿈을 이뤄주는 타이머, 꿈타"
+        },
+        servers: [
+            {
+                url: "http://localhost:8085"
+            },
+        ],
+    },
+    apis: ["./src/controller/*.js"]
+}
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 const debug = (req, res, next)=>{
     const beginTime = Date.now();
