47296d9c4e35dc4000c2ac19ead39b917e797285
김상현 <shkim7876@naver.com>
2023-08-10 23:16:53 +0900

    hotfix: removed unused console.log

--- device/backend/src/controller/stopwatchController.js
+++ device/backend/src/controller/stopwatchController.js
@@ -235,7 +235,6 @@ stopwatchRouter.put("/:group_id", (req, res)=>{
 stopwatchRouter.post("/operation/:group_id", (req, res)=>{
     const group_id = parseInt(req.params.group_id);
     const operation = req.body.operation;
-    console.log(operation);
     if(Object.is(group_id, NaN) || group_id < 0 || group_id > 5){
         res.status(400).json({status:`invalid group_id(${group_id})`});
         return;
