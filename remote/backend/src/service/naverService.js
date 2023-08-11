0b5c6ca55028ab8c5809756f25926b98ae9b7b53
김성민 <tjdalsdl19@gmail.com>
2023-08-11 15:45:39 +0900

    style: devide deploy develop axios address in social login part

--- remote/backend/src/service/naverService.js
+++ remote/backend/src/service/naverService.js
@@ -6,7 +6,8 @@ class NaverService {
     this.userRepository = new UserRepository();
     this.ID = process.env.NAVER_ID;
     this.SECRETE = process.env.NAVER_CLIENT_SECRETE;
-    this.redirect_URI = "http://localhost:3000/callback/naver";
+    // this.redirect_URI = "https://i9c101.p.ssafy.io/callback/naver"; // 배포용
+    this.redirect_URI = "http://localhost:3000/callback/naver"; // 개발용
   }
 
   getAuthCodeURL() {
