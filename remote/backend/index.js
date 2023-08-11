a291b2b6887a9070dc5661befb78151b94c41011
kunho <didrjsgh11@naver.com>
2023-08-11 10:33:54 +0900

    hotfix: nginx conf file fixed

--- remote/backend/index.js
+++ remote/backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const session = require("express-session");
 const passportConfig = require("./src/passport");
 const cookieParser = require("cookie-parser");
-const PORT = 8090;
+const PORT = 443;
 
 const authController = require("./src/controller/authController");
 const devController = require("./src/controller/devController");
@@ -65,5 +67,15 @@ app.use(cookieParser());
 app.use("/auth", authController);
 app.use("/dev", devController);
 app.use("/log", logController);
+
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/i9c101.p.ssafy.io/privateKey.pem", "utf8");
+const certificate = fs.readFileSync("/etc/letsencrypt/live/i9c101.p.ssafy.io/cert.pem", "utf8");
+const ca = fs.readFileSync("/etc/letsencrypt/live/i9c101.p.ssafy.io/chain.pem", "utf8");
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+}
+const httpsServer = https.createServer(credentials, app);
 
 app.listen(PORT, () => console.log(`Server listens on port ${PORT}`));
