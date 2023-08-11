bbed745b19945c9008c7796961a6290bf9c56b0e
kunho <didrjsgh11@naver.com>
2023-08-11 12:18:36 +0900

    hotfix: nginx conf file fixed

--- remote/frontend/src/components/login.js
+++ remote/frontend/src/components/login.js
@@ -64,7 +64,7 @@ export default function Login() {
   const googleURL = async () => {
     try {
       const { url } = await (
-        await fetch("https://localhost:8090/auth/google/url")
+        await fetch("https://i9c101.p.ssafy.io:8090/auth/google/url")
       ).json();
 
       console.log(url); // 응답으로 온 url
