298e7cfd48142cbe5849100f1e9b74d2f91f4b9e
kunho <didrjsgh11@naver.com>
2023-08-14 16:38:31 +0900

    feat: timerfromserverrepo 작성
    body:
    device에서 server db 데이터에 접근하는 repository 작성 -> component만

--- device/backend/src/repository/timerFromServerRepository.js
+++ device/backend/src/repository/timerFromServerRepository.js
@@ -5,6 +5,39 @@ class TimerFromServerRepository extends serverRepo {
     constructor(groupRepository) {
         super();
         this.groupRepository = groupRepository;
-        this.serial = new deviceRepo().getDeviceSerial();
+        this.serial = new deviceRepo().getDeviceSerial().device_serial;
+        this.userKey = this.getUserKeyBySerial();
+    }
+
+    async getUserKeyBySerial() {
+        const sql = `
+            SELECT user_key FROM user_tbl
+            JOIN device_tbl ON user_tbl.device_key = device_tbl.device_key
+            WHERE device_tbl.device_serial = ?;
+        `
+        const params = [this.serial];
+        const [rows] = await this.query(sql, params);
+        return rows[0].user_key;
+    }
+
+    async getAllGroup() {
+        const sql = `
+            SELECT * FROM component_tbl WHERE user_key = ?
+        `
+        const params = [this.userKey];
+        const [rows] = await this.query(sql, params);
+        return rows;
+    }
+
+    async getByGroupKey(group_id) {
+        const sql = `
+            SELECT component_key, init_Time, maxIter FROM component_tbl
+            WHERE user_key = ? AND group_key = ?
+        `
+        const params = [this.userKey, group_id];
+        const [rows] = await this.query(sql, params);
+        return rows;
     }
 }
+
+module.exports = TimerFromServerRepository;
