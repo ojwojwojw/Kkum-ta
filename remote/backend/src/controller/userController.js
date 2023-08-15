12268d6774ccba23e3e031aa25a20d85193e23fb
MayhemY <didrjsgh12@gmail.com>
2023-08-15 16:42:26 +0900

    feat: remote/backend userController file added

---
+++ remote/backend/src/controller/userController.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const Global = require("../global");
+const userRouter = express.Router();
+
+(async () => {
+    userRepository = await Global.getUserRepository();
+})();
+
+userRouter.get("/:serial", async (req, res) => {
+    const serial = req.params.serial;
+
+    res.json(await userRepository.getUserByDeviceSerial(serial));
+    return;
+});
+
+userRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    res.json(await userRepository.getUserById(id));
+    return;
+});
+
+userRouter.get("/:email", async (req, res) => {
+  const email = req.params.email;
+
+  res.json(await userRepository.getUserByEmail(email));
+  return;
+})
