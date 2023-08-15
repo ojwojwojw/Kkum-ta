4b87ffd3a17b1a42231e9225a1a6522bae65cf46
MayhemY <didrjsgh12@gmail.com>
2023-08-15 16:52:22 +0900

    hotfix: userController fixed

--- remote/backend/src/controller/userController.js
+++ remote/backend/src/controller/userController.js
@@ -6,23 +6,25 @@ const userRouter = express.Router();
     userRepository = await Global.getUserRepository();
 })();
 
-userRouter.get("/:serial", async (req, res) => {
+userRouter.get("/serial/:serial", async (req, res) => {
     const serial = req.params.serial;
 
     res.json(await userRepository.getUserByDeviceSerial(serial));
     return;
 });
 
-userRouter.get("/:id", async (req, res) => {
+userRouter.get("/id/:id", async (req, res) => {
     const id = req.params.id;
 
     res.json(await userRepository.getUserById(id));
     return;
 });
 
-userRouter.get("/:email", async (req, res) => {
+userRouter.get("/email/:email", async (req, res) => {
   const email = req.params.email;
 
   res.json(await userRepository.getUserByEmail(email));
   return;
 })
+
+module.exports = userRouter;
