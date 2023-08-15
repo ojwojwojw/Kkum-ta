c98a5e9bd92e2b02f712e73d4e5e784135185140
MayhemY <didrjsgh12@gmail.com>
2023-08-15 15:32:42 +0900

    commnet: device/backend synchroService file에 대한 주석 첨가

--- device/backend/src/service/synchroService.js
+++ device/backend/src/service/synchroService.js
@@ -1,6 +1,7 @@
 const global = require("../global");
 
 class SynchroService {
+    // server와 device의 group 및 component 관리 repo 가져오기
     constructor() {
         this.groupFromServerRepo = global.getGroupFromServerRepository();
         this.groupFromDeviceRepo = global.getGroupRepository();
@@ -9,19 +10,25 @@ class SynchroService {
     }
 
     async synchronizeDeviceAndServer() {
+        // device 사용자에 대한 모든 group 정보 get
         const groupData = this.groupFromServerRepo.getAll();
 
+        // 각각의 group에 대해
         groupData.map((item) => {
+            // gruop name, last_update 변경
             this.groupFromDeviceRepo.rename(item.group_key, item.name);
             this.groupFromDeviceRepo.setLastUpdate(
                 item.group_key,
                 item.last_update
             );
-
+            
+            // 현재 group에 대한 모든 component 정보 get
             const timerData = this.timerFromServerRepo.getByGroupId(
                 item.group_key
             );
+            // 기존 DB의 component 정보를 모두 삭제
             this.timerFromDeviceRepo.deleteAllByGroupKey(item.group_key);
+            // 새 component로 이식
             timerData.map((timer) => {
                 this.timerFromDeviceRepo.insert(
                     item.group_key,
